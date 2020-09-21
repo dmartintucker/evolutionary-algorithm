e00c1650d2a659c837c8e57a9e70852c87b8e850
D <d@Ds-MacBook-Pro.local>
2020-09-21 15:28:49 -0500

    Obj func now accepts dict input

--- genetic-algorithm/main.py
+++ genetic-algorithm/main.py
@@ -2,6 +2,7 @@
 # Global dependencies
 import sys
 import time
+import random
 import numpy as np
 from func_timeout import func_timeout, FunctionTimedOut
 
@@ -13,7 +14,7 @@ import matplotlib.pyplot as plt
 ###############################################################################
 
 
-class GeneticAlgorithm():
+class EvolutionaryAlgorithm():
 
     ###############################################################################
 
@@ -21,15 +22,17 @@ class GeneticAlgorithm():
                  algorithm_parameters={'max_num_iteration': None,
                                        'population_size': 100,
                                        'mutation_probability': 0.1,
-                                       'elit_ratio': 0.01,
+                                       'elite_ratio': 0.01,
                                        'crossover_probability': 0.5,
                                        'parents_portion': 0.3,
                                        'crossover_type': 'uniform',
                                        'max_iteration_without_improv': None}):
 
-        self.__name__ = GeneticAlgorithm
+        self.__name__ = EvolutionaryAlgorithm
         self.f = function
         self.dim = int(len(list(parameters)))
+        self.var_names = [[p['name']] for p in parameters]
+        # sys.stdout.write(str(self.var_names))
 
         # Check that parameters object is type dict
         assert(type(parameters) ==
@@ -47,13 +50,13 @@ class GeneticAlgorithm():
 
         for p in parameters:
             assert(
-                p['bounds']), "Error: every parameter item must have bounds"
+                p['bounds']), "\nError: every parameter item must have bounds"
             assert(
-                p['type']), "Error: every parameter item must have an explicit type"
+                p['type']), "\nError: every parameter item must have an explicit type"
 
             if p['type'] == 'bool':
                 assert(
-                    p['bounds'] == [0, 1]), "type 'bool' can only have bounds [0, 1]"
+                    p['bounds'] == [0, 1]), "\nError: type 'bool' can only have bounds [0, 1]"
 
         # Create variable bounds object
         self.var_bound = np.array([[x for x in p['bounds']]
@@ -67,35 +70,33 @@ class GeneticAlgorithm():
 
         self.pop_s = int(self.param['population_size'])
 
-        assert (self.param['parents_portion'] <= 1
-                and self.param['parents_portion'] >= 0),\
-            "parents_portion must be in range [0,1]"
+        assert (0 <= self.param['parents_portion'] <=
+                1), "\nError: argument 'parents_portion' must be in range [0,1]"
 
-        self.par_s = int(self.param['parents_portion']*self.pop_s)
-        trl = self.pop_s-self.par_s
+        self.par_s = int(self.param['parents_portion'] * self.pop_s)
+        trl = self.pop_s - self.par_s
         if trl % 2 != 0:
             self.par_s += 1
 
         self.prob_mut = self.param['mutation_probability']
 
-        assert (self.prob_mut <= 1 and self.prob_mut >= 0), \
-            "mutation_probability must be in range [0,1]"
+        assert (self.prob_mut <= 1 and self.prob_mut >=
+                0), "\nError: parameter 'mutation_probability' must be in range [0,1]"
 
         self.prob_cross = self.param['crossover_probability']
-        assert (self.prob_cross <= 1 and self.prob_cross >= 0), \
-            "mutation_probability must be in range [0,1]"
+        assert (self.prob_cross <= 1 and self.prob_cross >=
+                0), "\nError: parameter 'crossover_probability' must be in range [0,1]"
 
-        assert (self.param['elit_ratio'] <= 1 and self.param['elit_ratio'] >= 0),\
-            "elit_ratio must be in range [0,1]"
+        assert (self.param['elite_ratio'] <= 1 and self.param['elite_ratio'] >= 0),\
+            "\nError: parameter 'elite_ratio' must be in range [0,1]"
 
-        trl = self.pop_s*self.param['elit_ratio']
-        if trl < 1 and self.param['elit_ratio'] > 0:
+        trl = self.pop_s * self.param['elite_ratio']
+        if trl < 1 and self.param['elite_ratio'] > 0:
             self.num_elit = 1
         else:
             self.num_elit = int(trl)
 
-        assert(self.par_s >= self.num_elit), \
-            "\n number of parents must be greater than number of elits"
+        assert(self.par_s >= self.num_elit), "\nError: number of parents must be greater than number of generation elites"
 
         if self.param['max_num_iteration'] == None:
             self.iterate = 0
@@ -109,13 +110,15 @@ class GeneticAlgorithm():
             self.iterate = int(self.iterate)
             if (self.iterate*self.pop_s) > 10000000:
                 self.iterate = 10000000/self.pop_s
+            sys.stdout.write(
+                "\nMaximum iterations set to {}".format(str(self.iterate)))
         else:
             self.iterate = int(self.param['max_num_iteration'])
 
         self.c_type = self.param['crossover_type']
         assert (self.c_type == 'uniform' or self.c_type == 'one_point' or
                 self.c_type == 'two_point'),\
-            "\n crossover_type must 'uniform', 'one_point', or 'two_point' Enter string"
+            "\nError: parameter 'crossover_type' must be either 'uniform', 'one_point' or 'two_point'"
 
         self.stop_mniwi = False
         if self.param['max_iteration_without_improv'] == None:
@@ -127,25 +130,30 @@ class GeneticAlgorithm():
 
     def run(self):
 
-        self.integers = np.where(self.var_type == 'int')
-        self.reals = np.where(self.var_type == 'real')
-
         pop = np.array([np.zeros(self.dim+1)]*self.pop_s)
         solo = np.zeros(self.dim+1)
         var = np.zeros(self.dim)
 
         for p in range(0, self.pop_s):
 
-            for i in self.integers[0]:
-                var[i] = np.random.randint(self.var_bound[i][0],
-                                           self.var_bound[i][1]+1)
-                solo[i] = var[i].copy()
-            for i in self.reals[0]:
-                var[i] = self.var_bound[i][0]+np.random.random() *\
-                    (self.var_bound[i][1]-self.var_bound[i][0])
-                solo[i] = var[i].copy()
+            for i in range(len(self.var_type)):
+                if self.var_type[i][0] == 'int':
+                    var[i] = np.random.randint(self.var_bound[i][0],
+                                               self.var_bound[i][1]+1)
+                    solo[i] = var[i].copy()
+                elif self.var_type[i][0] == 'real':
+                    var[i] = self.var_bound[i][0]+np.random.random() *\
+                        (self.var_bound[i][1]-self.var_bound[i][0])
+                    solo[i] = var[i].copy()
+                else:
+                    var[i] = random.choice(self.var_bound[i])
+                    solo[i] = var[i].copy()
 
-            obj = self.sim(var)
+            vars = {self.var_names[j][0]: var[j]
+                    for j in range(len(var))}  # CHANGED
+            # sys.stdout.write(str(vars))
+
+            obj = self.sim(vars)
             solo[self.dim] = obj
             pop[p] = solo.copy()
 
@@ -219,17 +227,22 @@ class GeneticAlgorithm():
                 pvar2 = ef_par[r2, : self.dim].copy()
 
                 ch = self.cross(pvar1, pvar2, self.c_type)
+                #sys.stdout.write(str(ch)) #####
                 ch1 = ch[0].copy()
                 ch2 = ch[1].copy()
 
                 ch1 = self.mut(ch1)
                 ch2 = self.mutmidle(ch2, pvar1, pvar2)
                 solo[: self.dim] = ch1.copy()
-                obj = self.sim(ch1)
+                ch1_dict = {self.var_names[j][0]: var[j]
+                            for j in range(len(ch1))}
+                obj = self.sim(ch1_dict)
                 solo[self.dim] = obj
                 pop[k] = solo.copy()
                 solo[: self.dim] = ch2.copy()
-                obj = self.sim(ch2)
+                ch2_dict = {self.var_names[j][0]: var[j]
+                            for j in range(len(ch2))}
+                obj = self.sim(ch2_dict)
                 solo[self.dim] = obj
                 pop[k+1] = solo.copy()
 
@@ -255,11 +268,14 @@ class GeneticAlgorithm():
         self.output_dict = {'variable': self.best_variable, 'function':
                             self.best_function}
         show = ' '*100
+
+        self.best_params = {self.var_names[i][0]: self.best_variable[i] for i in range(
+            len(self.best_variable))}
         sys.stdout.write('\r%s' % (show))
-        sys.stdout.write('\r The best solution found:\n %s' %
-                         (self.best_variable))
-        sys.stdout.write('\n\n Objective function:\n %s\n' %
-                         (self.best_function))
+        sys.stdout.write(
+            '\nBest parameters found: {}'.format(str(self.best_params)))
+        sys.stdout.write('\nBest objective output = {}'.format(
+            str(self.best_function)))
         sys.stdout.flush()
         if self.stop_mniwi == True:
             sys.stdout.write(
@@ -301,46 +317,56 @@ class GeneticAlgorithm():
 
     def mut(self, x):
 
-        for i in self.integers[0]:
-            ran = np.random.random()
-            if ran < self.prob_mut:
-
-                x[i] = np.random.randint(self.var_bound[i][0],
-                                         self.var_bound[i][1]+1)
-
-        for i in self.reals[0]:
-            ran = np.random.random()
-            if ran < self.prob_mut:
-
-                x[i] = self.var_bound[i][0]+np.random.random() *\
-                    (self.var_bound[i][1]-self.var_bound[i][0])
+        for i in range(len(self.var_type)):
+            if self.var_type[i][0] == 'int':
+                ran = np.random.random()
+                if ran < self.prob_mut:
+                    x[i] = np.random.randint(self.var_bound[i][0],
+                                             self.var_bound[i][1]+1)
+                elif self.var_type[i][0] == 'real':
+                    x[i] = self.var_bound[i][0]+np.random.random() *\
+                        (self.var_bound[i][1]-self.var_bound[i][0])
+                else:
+                    x[i] = random.choice(self.var_bound[i])
 
         return x
 
 ###############################################################################
 
     def mutmidle(self, x, p1, p2):
-        for i in self.integers[0]:
-            ran = np.random.random()
-            if ran < self.prob_mut:
-                if p1[i] < p2[i]:
-                    x[i] = np.random.randint(p1[i], p2[i])
-                elif p1[i] > p2[i]:
-                    x[i] = np.random.randint(p2[i], p1[i])
-                else:
-                    x[i] = np.random.randint(self.var_bound[i][0],
-                                             self.var_bound[i][1]+1)
 
-        for i in self.reals[0]:
+        for i in range(len(self.var_type)):
             ran = np.random.random()
-            if ran < self.prob_mut:
-                if p1[i] < p2[i]:
-                    x[i] = p1[i]+np.random.random()*(p2[i]-p1[i])
-                elif p1[i] > p2[i]:
-                    x[i] = p2[i]+np.random.random()*(p1[i]-p2[i])
-                else:
-                    x[i] = self.var_bound[i][0]+np.random.random() *\
-                        (self.var_bound[i][1]-self.var_bound[i][0])
+
+            if self.var_type[i][0] == 'int':
+                if ran < self.prob_mut:
+                    if p1[i] < p2[i]:
+                        x[i] = np.random.randint(p1[i], p2[i])
+                    elif p1[i] > p2[i]:
+                        x[i] = np.random.randint(p2[i], p1[i])
+                    else:
+                        x[i] = np.random.randint(self.var_bound[i][0],
+                                                 self.var_bound[i][1]+1)
+
+            elif self.var_type[i][0] == 'real':
+                if ran < self.prob_mut:
+                    if p1[i] < p2[i]:
+                        x[i] = p1[i]+np.random.random()*(p2[i]-p1[i])
+                    elif p1[i] > p2[i]:
+                        x[i] = p2[i]+np.random.random()*(p1[i]-p2[i])
+                    else:
+                        x[i] = self.var_bound[i][0]+np.random.random() * \
+                            (self.var_bound[i][1]-self.var_bound[i][0])
+
+            else:
+                if ran < self.prob_mut:
+                    if p1[i] < p2[i]:
+                        x[i] = p1[i]+np.random.random()*(p2[i]-p1[i])
+                    elif p1[i] > p2[i]:
+                        x[i] = p2[i]+np.random.random()*(p1[i]-p2[i])
+                    else:
+                        x[i] = random.choice(self.var_bound[i])
+
         return x
 
 ###############################################################################
@@ -357,8 +383,8 @@ class GeneticAlgorithm():
             obj = func_timeout(self.funtimeout, self.evaluate)
         except FunctionTimedOut:
             print("given function is not applicable")
-        assert (obj != None), "After "+str(self.funtimeout)+" seconds delay " +\
-            "func_timeout: the given function does not provide any output"
+        assert (obj != None), "After {} seconds delay func_timeout: the given function does not provide any output".format(
+            str(self.funtimeout))
         return obj
 
 ###############################################################################
